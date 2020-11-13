f2d76849378d25094494c676f544e0c5f2b2e79a
Wei Zhang <iusenenow@gmail.com>
2020-11-13 21:56:54 +1100

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,23 @@
 const path = require('path')
+import { graphql } from 'gatsby';
 
 module.exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === "MarkdownRemark") {
     const slug = path.basename(node.fileAbsolutePath, '.md')
-    console.log('@@@@@@@@@@@@', slug);
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug
+    })
   }
+}
+
+module.exports.createPages = ({ graphql, actions }) => {
+  const { createPages } = actions
+  // 1. Get path to template
+  // 2. Get markdown data
+  // 3. Create new pages
+
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,12 +4,12 @@ import Footer from "../components/footer"
 import '../styles/index.scss'
 import layoutStyles from './layout.module.scss'
 
-const Layout = (props) => {
+const Layout = ({ children }) => {
   return (
     <div className={layoutStyles.container}>
       <div className={layoutStyles.content}>
         <Header />
-        {props.children}
+        {children}
       </div>
       <Footer />
     </div>
